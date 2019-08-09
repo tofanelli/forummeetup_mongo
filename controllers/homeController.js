8c9c95556040d24c909e3cdf1e2888b89c6ceb29
Fabrizio Tofanelli <fabrizio.tofanelli@gmail.com>
2019-08-09 09:28:58 -0300

    routes/homeRoute - atualizar produtos ok

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -33,10 +33,30 @@ exports.showAllProducts = async (request, response) => {
     .catch(error => console.error(error));
 };
 
+exports.updateProduct = async (request, response, next) => {
+  // Buscando no DB o id do produto a ser alterado
+  await Product.findByIdAndUpdate({
+    _id: request.params.id
+  }).then(product => {
+    // Recuperando os novos valores enviados
+    product.title = request.body.title;
+    product.description = request.body.description;
+
+    // Salvando no DB os novos valores e mandando uma mensagem de que
+    // a alteracao foi feita com sucesso
+    product.save().then(product => {
+      response.status(200).json({ message: "Produto alterado com sucesso" });
+    });
+  });
+};
+
 exports.deleteProduct = async (request, response, next) => {
-  Product.deleteOne({
+  // Metodo Mongo para exclusao de um item no db
+  await Product.deleteOne({
+    // Recuperando o valor do produto
     _id: request.params.id
   }).then(() => {
-    response.json({ message: "Produto excluido com sucesso" }).redirect("/");
+    response.json({ message: "Produto excluido com sucesso" });
+    // .redirect("/");
   });
 };
--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -5,12 +5,14 @@ const router = express.Router();
 const {
   createProduct,
   showAllProducts,
-  deleteProduct
+  deleteProduct,
+  updateProduct
 } = require("../controllers/homeController");
 
 // Rotas + middlewares
 router.post("/", createProduct);
 router.get("/", showAllProducts);
 router.delete("/:id", deleteProduct);
+router.put("/:id", updateProduct);
 
 module.exports = router;
